8b4b5e6c5e1868f3d29ffd8a50caf616a8d277dd
Angela Zhang <angelaz1@andrew.cmu.edu>
2020-12-28 21:12:45 -0600

    Adding experiences

--- js/index.js
+++ js/index.js
@@ -1,8 +1,72 @@
+/** Parsing Functions */
+/**
+ * Cleans a given string by removing all spaces and extraneous characters, then
+ * setting all lettres to lower case
+ * @param {string} text - text to clean
+ */
+function clean(text) {
+  return text.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "").toLowerCase();
+}
+
+/**
+ * Parses out project items according to the template in html/project.html and 
+ * adds to HTML file
+ * @param {string} filePath - file path to JSON containing project info
+ * @param {string} projectId - id of project section in HTML file
+ */
+function parseProjects(filePath, projectId) {
+  jQuery.get(filePath, function(data) {
+    // Create a cleaned title for the modal names
+    data.cards.forEach(card => {
+      card.cleanedTitle = clean(card.title);
+    });
+
+    var template = $("#project-card-template").html();
+    
+    var html = Mustache.render(template, data);
+    document.getElementById(projectId).innerHTML += html;
+  }, "json");
+}
+
+/**
+ * Parsing all project JSON files
+ */
+parseProjects("./assets/projects/software.json", "software-projects");
+parseProjects("./assets/projects/game.json", "game-projects");
+parseProjects("./assets/projects/class.json", "class-projects");
+parseProjects("./assets/projects/other.json", "other-projects");
+
+/**
+ * Parses out experience items according to the template in html/experience.html 
+ * and adds to HTML file
+ * @param {string} filePath - file path to JSON containing experience info
+ * @param {string} projectId - id of experiences section in HTML file
+ */
+function parseExperiences(filePath, projectId) {
+  jQuery.get(filePath, function(data) {
+
+    var template = $("#experience-template").html();
+    
+    var html = Mustache.render(template, data);
+    document.getElementById(projectId).innerHTML += html;
+  }, "json");
+}
+
+/**
+ * Parsing all experiences JSON files
+ */
+parseExperiences("./assets/experiences/experiences.json", "experiences");
+
+/** Scrolling Functions */
+
 const SCROLL_UP_THRESHOLD = 5;
 const SCROLL_DOWN_THRESHOLD = 20;
 var showingNavbar = true;
 var prevScrollpos = window.pageYOffset;
 
+/**
+ * When scrolling, hides or shows the navbar accordingly
+ */
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
 
@@ -18,30 +82,10 @@ window.onscroll = function() {
   prevScrollpos = currentScrollPos;
 }
 
-function clean(text) {
-  return text.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "").toLowerCase();
-}
-
-function parseProjects(filePath, projectId) {
-  jQuery.get(filePath, function(data) {
-    // Create a cleaned title for the modal names
-    data.cards.forEach(card => {
-      card.cleanedTitle = clean(card.title);
-    });
-
-    var template = $("#project-card-template").html();
-    
-    var html = Mustache.render(template, data);
-    document.getElementById(projectId).innerHTML += html;
-  }, "json");
-}
-
-parseProjects("./assets/projects/software.json", "software-projects");
-parseProjects("./assets/projects/game.json", "game-projects");
-parseProjects("./assets/projects/class.json", "class-projects");
-parseProjects("./assets/projects/other.json", "other-projects");
-
-// Perform scrolling animation when projects accordian is toggled
+/**
+ * Perform scrolling animation to the top of collapse header when projects 
+ * accordian is toggled
+ */
 $('.collapse.project').on('show.bs.collapse', function(e) {
   var $card = $(this).closest('.card');
   var $open = $($(this).data('parent')).find('.collapse.show');
@@ -65,15 +109,3 @@ $('.collapse.project').on('show.bs.collapse', function(e) {
     scrollTop: newTop
   }, 500); // TODO: USE TRANSFORM RATHER THAN CHANGING TOP
 });
-
-function parseExperiences(filePath, projectId) {
-    jQuery.get(filePath, function(data) {
-
-    var template = $("#experience-template").html();
-    
-    var html = Mustache.render(template, data);
-    document.getElementById(projectId).innerHTML += html;
-  }, "json");
-}
-
-parseExperiences("./assets/experiences/experiences.json", "experiences");
