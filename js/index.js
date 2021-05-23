3e705dc96036ddc7e8b4ce2f414948b82ba22466
Angela Zhang <angelaz1@andrew.cmu.edu>
2021-05-23 12:33:10 -0500

    Small UI changes

--- js/index.js
+++ js/index.js
@@ -58,16 +58,16 @@ window.onscroll = function() {
  */
 $(document).ready(function () {
   $('.fade-in').each(function(index) {
-      var top_of_element = $(this).offset().top;
-      var bottom_of_element = $(this).offset().top + $(this).outerHeight();
-      var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
-      var top_of_screen = $(window).scrollTop();
+    var top_of_element = $(this).offset().top;
+    var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+    var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+    var top_of_screen = $(window).scrollTop();
 
-      if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element) && !$(this).hasClass('is-visible')) {
-          $(this).delay(75 * index).queue(() => {
-              $(this).addClass('is-visible').dequeue();
-          });
-      }
+    if ((bottom_of_screen >= top_of_element) && (top_of_screen <= bottom_of_element) && !$(this).hasClass('is-visible')) {
+      $(this).delay(75 * index).queue(() => {
+        $(this).addClass('is-visible').dequeue();
+      });
+    }
   });
 })
 
@@ -76,14 +76,14 @@ $(document).ready(function () {
  */
 $(window).scroll(function() {
   $('.fade-in').each(function() {
-      var top_of_element = $(this).offset().top;
-      var bottom_of_element = $(this).offset().top + $(this).outerHeight();
-      var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
-      var top_of_screen = $(window).scrollTop();
+    var top_of_element = $(this).offset().top;
+    var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+    var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+    var top_of_screen = $(window).scrollTop();
 
-      if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element) && !$(this).hasClass('is-visible')) {
-          $(this).addClass('is-visible');
-      }
+    if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element) && !$(this).hasClass('is-visible')) {
+      $(this).addClass('is-visible');
+    }
   });
 });
 
